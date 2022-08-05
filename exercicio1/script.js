0f8e1776127c0ae6c2c15a5a6b3f184fb570f029
Luiz Dirceu <juninholdmarques@gmail.com>
2022-08-05 18:30:55 -0300

    atividade

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,5 +1,5 @@
-const num = //seu numero aqui
+const num = 246
 
-if(){
-    console.log("sua mensagem aqui");
+if(num % 2 === 0){
+    console.log("o numero é par")
 }
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,13 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
+let idade = 10
+let isTerminouOEnsinoMedio = false
+let isCursandoOutraFaculdade= false
 
-if(){
+if(idade>=18){console.log('a pessoa tem 18 anos ou mais')}
+else {console.log('nao tem 18 anos')}
 
-}else{
-	
-}
+if(isTerminouOEnsinoMedio){console.log('a pessoa terminou o ensino medio')}
+else {console.log('nao terminou o ensino medio')}
+
+if (isCursandoOutraFaculdade === false){ console.log('nao esta cursando outra faculdade')}
+else {console.log('esta cursando outra faculdade')}
+
